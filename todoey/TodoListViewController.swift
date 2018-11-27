9495addeacfcdae888273945fb9e2209a9824139
Basavaraj Soppannavar <BSS@basavarajs-macbook-pro.local>
2018-11-27 00:11:10 -0800

    data persisted

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    let itemArray = ["Find Mike", "Buy Eggs", "Destroy DEmogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggs", "Destroy DEmogorgon"]
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     // Mark table view methods
@@ -37,6 +45,32 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
+        }
+    // mark - add more items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+       let alert = UIAlertController(title: "Add New Items to Todoey", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what happens when user clicks add item button
+        
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.tableView.reloadData()   // to show items in table
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            //print(alertTextField.text!) triggers only in this clousure doesn't take the method field
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
         
     }
 }
