3d68e6388e33bf660285c6fcd687744e577fd397
Basavaraj Soppannavar <BSS@basavarajs-macbook-pro.local>
2018-11-29 23:45:44 -0800

    convert to mvc design pattern

--- todoey/Controllers/TodoListViewController.swift
+++ todoey/Controllers/TodoListViewController.swift
@@ -9,14 +9,30 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    var itemArray = ["Find Mike", "Buy Eggs", "Destroy DEmogorgon"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "car"
+        //newItem.done = true
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Bike"
+        //newItem.done = true
+       itemArray.append(newItem3)
+        
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
             itemArray = items
         }
         
@@ -28,21 +44,44 @@ class TodoListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+       
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        // ternary operator ==>
+        // value = condition ? valueTrue : valueFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        if item.done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
    
     }
     //mark - tableView delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
         //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark // only check mark
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+//
+        
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else
+//        {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
         }
@@ -56,7 +95,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what happens when user clicks add item button
         
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()   // to show items in table
---
+++ todoey/Data model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  todoey
+//
+//  Created by Basavaraj Soppannavar on 11/27/18.
+//  Copyright © 2018 Lakshmi B S. All rights reserved.
+//
+
+import Foundation
+
+class Item  {
+    var title : String = ""
+    var done : Bool = false
+}
